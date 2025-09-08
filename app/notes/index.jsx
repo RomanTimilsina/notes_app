c40ca1c568a3c2a458e9996363980671f00f69fe
RomanTimilsina <romantimilsina@gmail.com>
2025-09-08 16:58:54 +0545

    practise

---
+++ app/notes/index.jsx
@@ -0,0 +1,46 @@
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
+import { useState} from 'react'
+
+const NoteScreen = () => {
+    const [ notes, setNotes] = useState([
+        {id: 1, text: 'Note one'},
+        {id: 2, text: 'Note two'},
+        {id: 3, text: 'Note three'}
+    ])
+
+    return (
+        <View
+            style={styles.container}
+        >
+            <FlatList
+                data={notes}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => (
+                    <View style={ styles.noteItem }>
+                        <Text style={styles.noteText}>{ item.text }</Text>
+                    </View>
+                )}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container :{flex: 1,
+    padding: 20,
+    backgroundColor: '#fff'
+    },
+    noteItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor:'#f5f5f5',
+        padding: 15,
+        borderRadius: 5,
+        marginVertical: 5
+    },
+    noteText: {
+        fontSize: 18
+    }
+})
+
+export default NoteScreen;
